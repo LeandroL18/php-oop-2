9ec8065ba7c2a622c5257ad74913a6f7675d4629
LeandroL18 <leandrolipani6@gmail.com>
2024-02-06 15:29:42 +0100

    feat: class product

---
+++ models/product.php
@@ -0,0 +1,78 @@
+<?php
+
+    class Product {
+
+        private $id;
+
+        private $title;
+        private $image;
+        private $price;
+        
+        private $category;
+
+        // costruttore
+        public function __construct(
+            $id, $title, $image, $price,
+            Category $category
+        ) {
+
+            $this -> setId($id);
+            
+            $this -> setTitle($title);
+            $this -> setImage($image);
+            $this -> setPrice($price);
+            
+            $this -> setCategory($category);
+        }
+
+        // get e set id
+        public function getId() {
+
+            return $this -> id;
+        }
+        public function setId($id) {
+
+            $this -> id = $id;
+        }
+
+        // get e set title
+        public function getTitle() {
+
+            return $this -> title;
+        }
+        public function setTitle($title) {
+
+            $this -> title = $title;
+        }
+
+        // get e set image
+        public function getImage() {
+
+            return $this -> image;
+        }
+        public function setImage($image) {
+
+            $this -> image = $image;
+        }
+
+        // get e set price
+        public function getPrice() {
+
+            return $this -> price;
+        }
+        public function setPrice($price) {
+
+            $this -> price = $price;
+        }
+
+        // get e set category
+        public function getCategory() {
+
+            return $this -> category;
+        }
+        public function setCategory(Category $category) {
+
+            $this -> category = $category;
+        }
+    }
+?>
